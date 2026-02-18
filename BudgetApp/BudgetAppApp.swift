5c7b576c06f2d2f0f737b60a03175581ba9d2dc4
Valerie <valeriepieger@utexas.edu>
2026-02-18 11:24:27 -0600

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Valerie on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
